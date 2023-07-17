681fe9f59e2944fa06bd33147b65d92392ee4c78
toledomg <toledomg@yahoo.com.br>
2023-07-17 12:59:17 -0300

    :sparkles: feat: portfolio criado

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,112 @@
+// Styles
+import { Container, Flex } from '@/styles/Global';
+import { Text } from '@/styles/Text';
+import { Button } from '@/styles/Buttons';
+
+// Components
+import { Stack } from '@/components/Stack';
+import { Project } from '@/components/Project';
+import { Contacts } from '@/components/Contacts';
+
+// Data
+import { stackData } from '@/utils/stackData';
+import { userData } from '@/utils/userData';
+
+import { FaGithub } from 'react-icons/fa';
+
+// Page Style
+import {
+  Header,
+  HeaderContent,
+  HeaderButtonsArea,
+  UserImage,
+  StackCards,
+  ProjectsArea,
+  ProjectsAreaSocialMediaMessage,
+  ProjectAreaWrapperColumns,
+  ProjectsAreaContent,
+} from './style';
+
+export const Home = (): JSX.Element => {
+  const gihubUrl = `https://github.com/${userData.githubUser}`;
+  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+
+  return (
+    <main id="home">
+      <Header>
+        <Container>
+          <HeaderContent>
+            <Flex>
+              <UserImage
+                src={`https://github.com/${userData.githubUser}.png`}
+                alt={userData.nameUser}
+                title={userData.nameUser}
+                width={'48px'}
+                height={'48px'}
+              />
+              <Text color="grey4"> Olá, me chamo {userData.nameUser}</Text>
+            </Flex>
+            <Text as="h1" type="heading1" color="grey5">
+              Eu{' '}
+              <Text as="span" type="heading1" color="brand1">
+                amo
+              </Text>{' '}
+              criar e{' '}
+              <Text as="span" type="heading1" color="brand1">
+                desenvolver
+              </Text>{' '}
+              projetos
+            </Text>
+            <Text type="body1" color="grey2">
+              Descubra nesse portfolio, criado especialmente para poderem ver
+              alguns de meus projetos e tecnologias.
+            </Text>
+            <HeaderButtonsArea>
+              <Button as="a" type="primary" href="#projects">
+                Ver Projetos
+              </Button>
+              {/* <Button as="a" type="outline" target="_blank" href={portfolioUrl}>
+                Meu portfolio de Código fonte
+              </Button> */}
+              <Button
+                color="grey5"
+                as="a"
+                css={{ '&:hover': { color: '$grey1' } }}
+                type="circle"
+                target="_blank"
+                href={gihubUrl}>
+                <FaGithub />
+              </Button>
+            </HeaderButtonsArea>
+            <StackCards>
+              {stackData.map((stack, index) => (
+                <Stack key={index} title={stack.title} icon={stack.img} />
+              ))}
+            </StackCards>
+          </HeaderContent>
+        </Container>
+      </Header>
+      <ProjectsArea id="projects">
+        <Container>
+          <ProjectAreaWrapperColumns>
+            <ProjectsAreaSocialMediaMessage>
+              <Text as="h2" type="heading4" color="grey4">
+                Projetos
+              </Text>
+              <Text as="p" type="body1" color="grey2">
+                Alguns de meus{' '}
+                <Text as="span" color="brand5">
+                  projetos
+                </Text>
+              </Text>
+            </ProjectsAreaSocialMediaMessage>
+            <ProjectsAreaContent>
+              <Project />
+            </ProjectsAreaContent>
+          </ProjectAreaWrapperColumns>
+        </Container>
+      </ProjectsArea>
+      <Contacts />
+    </main>
+  );
+};
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: 'Alexsandro',
+  githubUser: 'toledomg',
+  linkedinUser: 'toledomg',
+  whatsappNumber: '+5534988075375',
+  emailUser: 'contato@luchost.com',
+  instagramUser: 'oalextoledo',
+  facebookUser: 'toledomg',
+};
